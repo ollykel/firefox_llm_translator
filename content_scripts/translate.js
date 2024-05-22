db2062effebcfdb70cce85310fd584ddd1dcebe6
Oliver A. Kelton <oakelton@gmail.com>
2024-05-22 16:42:34 -0700

    content_scripts/translate.js : implemented ElementVisitor.getTagName()

--- content_scripts/translate.js
+++ content_scripts/translate.js
@@ -132,6 +132,7 @@
 
                 const visitor = {
                     "getUID": () => uid,
+                    "getTagName": () => element.tagName,
                     "getOrigContent": () => origContent,
                     "getTranslatedContent": () => translatedText,
                     "setTranslatedContent": (newContent) => { translatedContent = newContent; },
