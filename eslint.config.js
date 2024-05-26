3f6ad0209e93b39097f591bc6c2f69f80b8a72fa
Oliver A. Kelton <oakelton@gmail.com>
2024-05-26 14:43:00 -0700

    created eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    "rules": {
+      "semi": "error",
+      "prefer-const": "warn",
+      "no-undef": "error"
+    },
+    "languageOptions": {
+      "globals": {
+        "console": "readable",
+        "alert": "readable",
+        "document": "readable",
+        "window": "readable",
+        "browser": "readable"
+      }
+    }
+  }
+];
