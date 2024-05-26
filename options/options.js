4bb0edcad1cec6f3344d601db15f4d064b4fdf58
Oliver A. Kelton <oakelton@gmail.com>
2024-05-26 12:17:23 -0700

    options/options.js : re-implemented getFormValues using new function getInputValue

--- options/options.js
+++ options/options.js
@@ -48,22 +48,94 @@ const getFormInputs = (form) =>
     return nameToInputMap;
 };// end getFormInputs
 
+const getInputValue = (input) =>
+{
+    switch (input.type)
+    {
+        case 'button':
+        case 'submit':
+        case 'reset':
+            return null;
+        case 'checkbox':
+            if (input.checked)
+            {
+                return [input.name, [input.value]];
+            } else {
+                return null;
+            }
+        case 'radio':
+            if (input.checked)
+            {
+                return [input.name, input.value];
+            } else {
+                return null;
+            }
+        case 'color':
+            return [input.name, input.value];
+        case 'date':
+            return [input.name, new Date(input.value)];
+        case 'datetime-local':
+            return [input.name, new Date(input.value)];
+        case 'email':
+            return [input.name, input.value];
+        case 'file':
+            return [input.name, input.files];
+        case 'hidden':
+            return [input.name, input.value];
+        case 'image':
+            return [input.name, input.src];
+        case 'month':
+            return [input.name, new Date(input.value)];
+        case 'number':
+            return [input.name, Number(input.value)];
+        case 'password':
+            return [input.name, input.value];
+        case 'range':
+            return [input.name, Number(input.value)];
+        case 'search':
+            return [input.name, input.value];
+        case 'tel':
+            return [input.name, input.value];
+        case 'text':
+            return [input.name, input.value];
+        case 'time':
+            return [input.name, new Date(input.value)];
+        case 'url':
+            return [input.name, input.value];
+        case 'week':
+            return [input.name, new Date(input.value)];
+        default:
+            return null;
+    }
+}// end getInputValue
+
 const getFormValues = (form) =>
 {
     const nameToInputMap = getFormInputs(form);
-
-    return Object.fromEntries(Object.entries(nameToInputMap).map(([k, input]) =>
-    {
-        switch (input.type)
+    const nameValuePairs = Object.values(nameToInputMap)
+        .map(getInputValue)
+        .reduce((accum, kv) =>
         {
-            case 'number':
-                return [k, Number(input.value)];
-            case 'submit':
-                return null;
-            default:
-                return [k, input.value];
-        }// end switch (input.type)
-    }).filter((entry) => entry !== null));
+            if (kv !== null)
+            {
+                const [name, val] = kv;
+
+                if (name in accum)
+                {
+                    accum[name].push(...val);
+                }
+                else
+                {
+                    accum[name] = val;
+                }
+            }
+
+            return accum;
+        },
+        {}
+    );
+
+    return nameValuePairs;
 };// end getFormValues
 
 const handleSubmit = (ev) =>
