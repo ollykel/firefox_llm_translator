3f553a3b6bd7949ff173db8ded593085a7d162ea
Oliver A. Kelton <oakelton@gmail.com>
2024-05-25 18:48:12 -0700

    created simple implementation of settings panel

---
+++ options/options.js
@@ -0,0 +1,66 @@
+// Handles setting options.
+
+const OPTION_DEFAULTS =
+{
+    "model": "gpt-3.5-turbo",
+    "apiKey": "",
+    "temperature": 0.7
+};
+
+const logError = (e) =>
+{
+    console.log(`ERROR: ${e}`);
+};// end logError
+
+const extractObj = (obj, ...fields) =>
+{
+    let currObj = obj;
+
+    for (const field of fields)
+    {
+        if (currObj.hasOwnProperty(field))
+        {
+            currObj = currObj[field];
+        }
+        else
+        {
+            return null;
+        }
+    }// end for (let field of fields)
+
+    return currObj;
+};// end extractObj
+
+const getFormValues = (form) =>
+{
+    const inputs = form.elements;
+    let output = {};
+
+    for (let i = 0; i < inputs.length; ++i)
+    {
+        const currInput = inputs[i];
+
+        if (currInput.nodeName === 'INPUT')
+        {
+            const inputName = currInput.name;
+            const inputValue = currInput.value;
+            
+            output[inputName] = inputValue;
+        }
+    }// end for (let i = 0; i < inputs.length; ++i)
+    
+    return output;
+};// end getFormValues
+
+const handleSubmit = (ev) =>
+{
+    ev.preventDefault();
+
+    const formValues = getFormValues(ev.target);
+
+    browser.storage.sync.set(formValues);
+    alert('Extension settings saved.');
+};// end handleSubmit
+
+// === add listeners ===========================================================
+document.getElementById("settings-form").addEventListener("submit", handleSubmit);
