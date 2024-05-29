67ba6bbf97dd18b7f3c7b993c1adf6ca32b2a53e
Oliver A. Kelton <oakelton@gmail.com>
2024-05-29 16:22:53 -0700

    added utils.js to webpack entry points

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const SRC_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
     entry: {
+        'utils': path.resolve(SRC_DIR, 'utils.js'),
         'content_scripts/translate': path.resolve(SRC_DIR, 'content_scripts', 'translate.js'),
         'popup/translate': path.resolve(SRC_DIR, 'popup', 'translate.js'),
         'options/options': path.resolve(SRC_DIR, 'options', 'options.js')
