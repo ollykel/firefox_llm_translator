acb5eda683e178ab42673e357aae537b6f700fe6
Oliver A. Kelton <oakelton@gmail.com>
2024-05-29 10:43:42 -0700

    popup : added settings loading to translate.js

--- popup/translate.js
+++ popup/translate.js
@@ -1,5 +1,7 @@
 // Inject the content script at popup load time.
 
+const KEY_API_SETTINGS = 'apiSettings';
+
 const apiConfigBase = {
     endpoint: 'https://api.openai.com/v1/chat/completions',
     model: "gpt-3.5-turbo",
@@ -11,7 +13,7 @@ const extractObj = (obj, ...fields) =>
 {
     let currObj = obj;
 
-    for (let field of fields)
+    for (const field of fields)
     {
         if (currObj.hasOwnProperty(field))
         {
@@ -26,26 +28,155 @@ const extractObj = (obj, ...fields) =>
     return currObj;
 };// end extractObj
 
+const logError = (e) =>
+{
+    console.log(`ERROR: ${e}`);
+};// end logError
+
+const getFormInputs = (form) =>
+{
+    const out = [];
+
+    for (const elem of form.elements)
+    {
+        if (elem.nodeName === 'INPUT')
+        {
+            out.push(elem);
+        }
+    }// end for (const elem of form.elements)
+    
+    return out;
+};// end getFormInputs
+
+const getInputValue = (input) =>
+{
+    switch (input.type)
+    {
+        case 'button':
+        case 'submit':
+        case 'reset':
+            return null;
+        case 'checkbox':
+            if (input.checked)
+            {
+                return [input.name, [input.value]];
+            } else {
+                return null;
+            }
+        case 'radio':
+            if (input.checked)
+            {
+                return [input.name, input.value];
+            } else {
+                return null;
+            }
+        case 'color':
+            return [input.name, input.value];
+        case 'date':
+            return [input.name, new Date(input.value)];
+        case 'datetime-local':
+            return [input.name, new Date(input.value)];
+        case 'email':
+            return [input.name, input.value];
+        case 'file':
+            return [input.name, input.files];
+        case 'hidden':
+            return [input.name, input.value];
+        case 'image':
+            return [input.name, input.src];
+        case 'month':
+            return [input.name, new Date(input.value)];
+        case 'number':
+            return [input.name, Number(input.value)];
+        case 'password':
+            return [input.name, input.value];
+        case 'range':
+            return [input.name, Number(input.value)];
+        case 'search':
+            return [input.name, input.value];
+        case 'tel':
+            return [input.name, input.value];
+        case 'text':
+            return [input.name, input.value];
+        case 'time':
+            return [input.name, new Date(input.value)];
+        case 'url':
+            return [input.name, input.value];
+        case 'week':
+            return [input.name, new Date(input.value)];
+        default:
+            return null;
+    }
+};// end getInputValue
+
 const getFormValues = (form) =>
 {
-    const inputs = form.elements;
-    let output = {};
-
-    for (let i = 0; i < inputs.length; ++i)
-    {
-        const currInput = inputs[i];
-
-        if (currInput.nodeName === 'INPUT')
+    const inputs = getFormInputs(form);
+    const nameValuePairs = inputs
+        .map(getInputValue)
+        .reduce((accum, kv) =>
         {
-            const inputName = currInput.name;
-            const inputValue = currInput.value;
-            
-            output[inputName] = inputValue;
-        }
-    }// end for (let i = 0; i < inputs.length; ++i)
-    
-    return output;
+            if (kv !== null)
+            {
+                const [name, val] = kv;
+
+                if (name in accum)
+                {
+                    accum[name].push(...val);
+                }
+                else
+                {
+                    accum[name] = val;
+                }
+            }
+
+            return accum;
+        },
+        {}
+    );
+
+    return nameValuePairs;
 };// end getFormValues
+;// end getFormValues
+
+const setFormInputs = (form, nameToValueMap) =>
+{
+    const inputs = getFormInputs(form);
+
+    for (const input of inputs)
+    {
+        const name = input.name;
+
+        if (name in nameToValueMap)
+        {
+            const val = nameToValueMap[name];
+
+            if (input.type === 'checkbox')
+            {
+                input.checked = val.include(input.value);
+            }
+            else if (input.type === 'radio')
+            {
+                input.checked = (input.value === val);
+            }
+            else
+            {
+                input.value = val;
+            }
+        }
+    }// end for (const input of inputs)
+};// end setFormInputs
+
+const loadSettings = () =>
+{
+    const form = document.getElementById("translate-form");
+
+    browser.storage.sync.get(KEY_API_SETTINGS)
+        .then(
+            (nameToValueMap) => setFormInputs(form, nameToValueMap[KEY_API_SETTINGS]),
+            logError
+        );
+};// end loadSettings
 
 const addEventListeners = () =>
 {
@@ -60,7 +191,7 @@ const addEventListeners = () =>
         
         const form = e.target;
         const inputValues = getFormValues(form);
-        const apiKey = inputValues['api-key'];
+        const apiKey = inputValues['apiKey'];
         const targetLanguage = inputValues['target-language'];
         const characterLimit = Number(inputValues['character-limit']);
         
@@ -160,3 +291,6 @@ browser.runtime.onMessage.addListener((message) =>
         notifyRequestProcessingFinished();
     }
 });
+
+// load settings
+window.addEventListener("load", loadSettings);
