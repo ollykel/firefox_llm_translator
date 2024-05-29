8de5aa8618877363c5bb59ae6f2f1ed49fd26a38
Oliver A. Kelton <oakelton@gmail.com>
2024-05-29 16:59:12 -0700

    moved extractObj, getFormInputs, getInputValues, getFormValues, setFormInputs to utils.js from options/options.js

--- src/options/options.js
+++ src/options/options.js
@@ -5,162 +5,18 @@ const {
   OPTION_DEFAULTS
 } = require('../config.json');
 
+const {
+  extractObj,
+  getFormInputs,
+  getInputValue,
+  getFormValues,
+  setFormInputs
+} = require('../utils.js');
+
 const logError = (e) =>
 {
     console.log(`ERROR: ${e}`);
 };// end logError
-
-const extractObj = (obj, ...fields) =>
-{
-    let currObj = obj;
-
-    for (const field of fields)
-    {
-        if (currObj.hasOwnProperty(field))
-        {
-            currObj = currObj[field];
-        }
-        else
-        {
-            return null;
-        }
-    }// end for (let field of fields)
-
-    return currObj;
-};// end extractObj
-
-const getFormInputs = (form) =>
-{
-    const out = [];
-
-    for (const elem of form.elements)
-    {
-        if (elem.nodeName === 'INPUT')
-        {
-            out.push(elem);
-        }
-    }// end for (const elem of form.elements)
-    
-    return out;
-};// end getFormInputs
-
-const getInputValue = (input) =>
-{
-    switch (input.type)
-    {
-        case 'button':
-        case 'submit':
-        case 'reset':
-            return null;
-        case 'checkbox':
-            if (input.checked)
-            {
-                return [input.name, [input.value]];
-            } else {
-                return null;
-            }
-        case 'radio':
-            if (input.checked)
-            {
-                return [input.name, input.value];
-            } else {
-                return null;
-            }
-        case 'color':
-            return [input.name, input.value];
-        case 'date':
-            return [input.name, new Date(input.value)];
-        case 'datetime-local':
-            return [input.name, new Date(input.value)];
-        case 'email':
-            return [input.name, input.value];
-        case 'file':
-            return [input.name, input.files];
-        case 'hidden':
-            return [input.name, input.value];
-        case 'image':
-            return [input.name, input.src];
-        case 'month':
-            return [input.name, new Date(input.value)];
-        case 'number':
-            return [input.name, Number(input.value)];
-        case 'password':
-            return [input.name, input.value];
-        case 'range':
-            return [input.name, Number(input.value)];
-        case 'search':
-            return [input.name, input.value];
-        case 'tel':
-            return [input.name, input.value];
-        case 'text':
-            return [input.name, input.value];
-        case 'time':
-            return [input.name, new Date(input.value)];
-        case 'url':
-            return [input.name, input.value];
-        case 'week':
-            return [input.name, new Date(input.value)];
-        default:
-            return null;
-    }
-};// end getInputValue
-
-const getFormValues = (form) =>
-{
-    const inputs = getFormInputs(form);
-    const nameValuePairs = inputs
-        .map(getInputValue)
-        .reduce((accum, kv) =>
-        {
-            if (kv !== null)
-            {
-                const [name, val] = kv;
-
-                if (name in accum)
-                {
-                    accum[name].push(...val);
-                }
-                else
-                {
-                    accum[name] = val;
-                }
-            }
-
-            return accum;
-        },
-        {}
-    );
-
-    return nameValuePairs;
-};// end getFormValues
-
-const setFormInputs = (form, nameToValueMap) =>
-{
-    const inputs = getFormInputs(form);
-
-    for (const input of inputs)
-    {
-        const name = input.name;
-
-        if (name in nameToValueMap)
-        {
-            const val = nameToValueMap[name];
-
-            if (input.type === 'checkbox')
-            {
-                input.checked = val.include(input.value);
-            }
-            else if (input.type === 'radio')
-            {
-                input.checked = (input.value === val);
-            }
-            else
-            {
-                input.value = val;
-            }
-        }
-    }// end for (const input of inputs)
-};// end setFormInputs
 
 const loadSettings = () =>
 {
