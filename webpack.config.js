0f61f95de1f6e9b6ac166ce3f3af4a8c55a8fe30
Oliver A. Kelton <oakelton@gmail.com>
2024-05-29 15:57:55 -0700

    created webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+    entry: {
+        'content_scripts/translate': path.resolve(SRC_DIR, 'content_scripts', 'translate.js'),
+        'popup/translate': path.resolve(SRC_DIR, 'popup', 'translate.js'),
+        'options/options': path.resolve(SRC_DIR, 'options', 'options.js')
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'target')
+    }
+};
